5651e55d01dea7a471d08140a955f655469c1382
changsuwi <F74036255@mail.ncku.edu.tw>
2017-05-06 17:55:57 +0800

    add location

--- app.py
+++ app.py
@@ -46,7 +46,8 @@ def webhook():
                 if messaging_event.get("message"):  # someone sent us a message
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    if(messaging_event["message"].has_key("payload")):
+                    if(messaging_event["message"].has_key("quick_reply")):
+                        payload = messaging_event["message"]["quick_reply"]["payload"]
                         if(payload == "mainbutton1"):
                             json_location(sender_id)
                     elif(messaging_event["message"].has_key("text")):
