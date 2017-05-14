e17d8cc3ab8e199c4be0ce620585d93b6eeb4b40
changsuwi <F74036255@mail.ncku.edu.tw>
2017-05-14 15:18:15 +0800

    fix bug

--- db.py
+++ db.py
@@ -32,7 +32,7 @@ def search_scene(sender_id, px, py, count2):
     template = new_template(sender_id)
     count = 0
     for doc in scenes.find():
-        if (pow(float(doc['Px']) - px, 2) + pow(float(doc['Py']) - py, 2) < 0.35):
+        if (pow(float(doc['Px']) - px, 2) + pow(float(doc['Py']) - py, 2) < 0.035):
             count = count + 1
             if(count >= count2):
                 name = doc['Name'].encode('utf-8')
